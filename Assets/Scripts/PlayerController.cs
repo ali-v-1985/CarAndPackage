1529cb44b9fff2a6476a2d7477433b5d2a130bbc
Ali <ali.valizadeh.h@gmail.com>
2020-07-15 00:09:46 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 35;
+
+    public float Speed
+    {
+        get => speed;
+        set => speed = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * (Time.deltaTime * Speed));
+    }
+}
