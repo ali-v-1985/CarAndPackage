3e15f1b162f6f76ede99ea7c5558b0de2d41bf80
Ali <ali.valizadeh.h@gmail.com>
2020-07-15 01:40:57 +0200

    Turn vehicle instead of slide

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,22 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField]
-    private float speed = 35;
-
+    private float speed = 20;
     public float Speed
     {
         get => speed;
         set => speed = value;
     }
 
+    [SerializeField] private float turnSpeed = 50;
+    public float TurnSpeed
+    {
+        get => turnSpeed;
+        set => turnSpeed = value;
+    }
+
+    private float _horizontalInput;
+    private float _accelerationInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +32,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * (Time.deltaTime * Speed));
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _accelerationInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * (Time.deltaTime * Speed * _accelerationInput));
+        if (_accelerationInput > 0)
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * TurnSpeed * _horizontalInput);
+        }
     }
 }
